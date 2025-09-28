1e054607f0b07f7e972b3051f72f0f46fa9c990d
noName <samyrize77777@gmail.com>
2025-09-29 03:00:50 +0400

    ios

--- src/components/SilkBackground.tsx
+++ src/components/SilkBackground.tsx
@@ -23,15 +23,6 @@ const SilkBackground: React.FC<SilkBackgroundProps> = ({
     <div
       className="silk-background"
       style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        minHeight: '100vh',
-        minHeight: '100dvh',
-        minHeight: '-webkit-fill-available',
-        zIndex: -1,
         transform: `rotate(${rotation}deg)`,
         ...style
       }}
