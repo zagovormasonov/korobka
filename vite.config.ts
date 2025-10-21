fb7e43775d3feffb8eae8f1be124043c16cc3cd6
Иван Ардашев <ivanardashev@MacBook-Air-Mac.local>
2025-10-22 01:02:42 +0400

    Исправить кэширование: добавить хэширование файлов и отладку для анимации заявки

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,14 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser'
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
   },
   server: {
     port: 3000,
